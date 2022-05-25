e8db151ec7bd038c82b755edbe497d1d0f9c8d44
193040126 <193040126.diva@mail.unpas.ac.id>
2022-05-25 19:48:56 +0700

    Mengubah halaman home fragmnet menjadi halaman tutorial dengan menambah kan gambar animasi menggunakan library Lottie dan juga memberikan keterangan nya

--- app/src/main/java/com/dicoding/diva/pimpledetectku/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dicoding/diva/pimpledetectku/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.diva.pimpledetectku.databinding.FragmentHomeBinding
@@ -32,6 +31,7 @@ class HomeFragment : Fragment() {
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
         return root
     }
 
@@ -39,4 +39,5 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
